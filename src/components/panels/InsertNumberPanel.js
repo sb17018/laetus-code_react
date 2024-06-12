02f5428c8b45b78a46fd7f90d88d0f6c3957f77f
sb17018 <piotr.a.bar@gmail.com>
2024-06-12 16:51:41 +0200

    Stylize more

--- src/components/panels/InsertNumberPanel.js
+++ src/components/panels/InsertNumberPanel.js
@@ -4,7 +4,7 @@ function InsertNumberPanel() {
     return <div>
         <h4>Enter a number to generate code</h4>
         <div id="toEnterNumber">
-            <input type="type" id={selectors.userInputField} placeholder="REQUIRED MINIMAL VALUE 3" onCopy="false" />
+            <input type="number" id={selectors.userInputField} placeholder="REQUIRED MINIMAL VALUE 3" onCopy="false" />
         </div>
     </div>;
 }
