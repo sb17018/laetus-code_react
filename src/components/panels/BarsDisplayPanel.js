236eeff7a6cfd939ab67934224c59d9895be01d3
sb17018 <piotr.a.bar@gmail.com>
2024-06-15 14:23:15 +0200

    Make numberic code display

--- src/components/panels/BarsDisplayPanel.js
+++ src/components/panels/BarsDisplayPanel.js
@@ -11,86 +11,21 @@ function BarsDisplayPanel() {
     // all existing bars parameters array
     const barsParams = barsCombCtx.eachBarParams;
 
-    // barsParams.forEach(elem=>console.log(elem.position + " " + elem.value));
+    const barsValue = barsCombCtx.barsValue;
 
-    // const BARS_DISPLAY = document.querySelector("#barsPlaceholder");
-
-    // function displayAllBars() {
-    //     let theBarsComb = "";
-    //     BARS_DISPLAY.style.width = barCodeCurrenWidth() + "px";
-    //     // if (
-    //     //     theBarsCombDisplay.barCodeCurrenWidth() > theBarsCombDisplay.codeFieldWidth
-    //     // ) {
-    //     //     let barCodeFieldWidth = parseInt(
-    //     //         document.querySelector("#barsPlaceholder").style.width
-    //     //     );
-    //     //     document.querySelector("#barsCombPlaceholder").style.width =
-    //     //         barCodeFieldWidth + 20 + "px";
-    //     //     document.querySelector("#mainField").style.width =
-    //     //         barCodeFieldWidth + 100 + "px";
-    //     // } else {
-    //     //     document.querySelector("#barsCombPlaceholder").style.width = "320px";
-    //     //     document.querySelector("#mainField").style.width = "400px";
-    //     // }
-
-    //     barsParams.forEach((elem) => {
-    //         theBarsComb += defineTheBar(elem.position, elem.value);
-    //     });
-    //     if (theBarsComb != "") BARS_DISPLAY.innerHTML = theBarsComb;
-    // }
-    // function displayAllBars() {
-    //     let theBarsComb = "";
-    //     document.querySelector("#barsPlaceholder").style.width =
-    //         theBarsCombDisplay.barCodeCurrenWidth() + "px";
-    //     if (
-    //         theBarsCombDisplay.barCodeCurrenWidth() > theBarsCombDisplay.codeFieldWidth
-    //     ) {
-    //         let barCodeFieldWidth = parseInt(
-    //             document.querySelector("#barsPlaceholder").style.width
-    //         );
-    //         document.querySelector("#barsCombPlaceholder").style.width =
-    //             barCodeFieldWidth + 20 + "px";
-    //         document.querySelector("#mainField").style.width =
-    //             barCodeFieldWidth + 100 + "px";
-    //     } else {
-    //         document.querySelector("#barsCombPlaceholder").style.width = "320px";
-    //         document.querySelector("#mainField").style.width = "400px";
-    //     }
-
-    //     theBarsCombDisplay.eachBarSpecs.forEach((elem) => {
-    //         theBarsComb += defineTheBar(elem.position, elem.value);
-    //     });
-    //     if (theBarsComb != "") theBars.innerHTML = theBarsComb;
-    // }
-
-    // function barCodeCurrenWidth(){
-    //     return barsParams[barsParams.length -1].position + barsParams[barsParams.length -1].value;
-    // }
-
-    // function defineTheBar(theBarPosition, theBarWidth) {
-    //     return (
-    //         '<rect x="' +
-    //         theBarPosition +
-    //         '" y="15" width="' +
-    //         theBarWidth +
-    //         '" height="120" fill="black"/>'
-    //     );
-    // }
-
-    // function displayAllBars(){
-
-    // };
+    function barCodeCurrenWidth() {
+        return barsParams.length > 0 ? barsParams[barsParams.length - 1].position + barsParams[barsParams.length - 1].value : 0;
+    }
 
     const BARS_TO_DISPLAY = [];
-    barsParams.forEach((elem) => BARS_TO_DISPLAY.push(<Bar position={elem.position} width={elem.value} />));
+    barsParams.forEach((elem, index) => BARS_TO_DISPLAY.push(<Bar key={index} position={elem.position} width={elem.value} />));
 
 
     return <div id={selectors.barsPlaceholderPanel}>
-        <svg xmlns="http://www.w3.org/2000/svg" className={selectors["bars-placeholder"]} id="barsPlaceholder">
-        {BARS_TO_DISPLAY}
-        {/* {barsParams.forEach((elem) => console.log(elem.position + " " + elem.value))} */}
+        <svg xmlns="http://www.w3.org/2000/svg" className={selectors["bars-placeholder"]} id="barsPlaceholder" style={{ width: barCodeCurrenWidth() + "px" }}>
+            {BARS_TO_DISPLAY}
         </svg>
-        <div id={selectors.calculationPlaceholderFrame}><output id={selectors.calculationPlaceholder}></output></div>
+        <div id={selectors.calculationPlaceholderFrame}><output id={selectors.calculationPlaceholder} className={barsValue > 2 ? selectors["code-number"] : ""}>{barsValue > 2 ? barsValue : ""}</output></div>
     </div>;
 }
 
--- src/js/barGeneration-context.js
+++ src/js/barGeneration-context.js
@@ -8,13 +8,17 @@ const BarGenerationCtx = createContext({
     modul: 10,
 
     eachBarParams: [],
+    barsValue: 0,
     enterNextBar: () => { },
     removeLastBar: () => { },
     removeAllBars: () => { },
 });
 
+let barsCalculatedValue = 0;
+
 export function BarGenerationCtxProvider(props) {
     const [barsCombination, setBarsCombination] = useState([]);
+    // const [barsCalculatedValue, setbarsCalculatedValue] = useState(0);
 
     // to add a bar
     function enterNextBarHandler(barType) {
@@ -22,9 +26,14 @@ export function BarGenerationCtxProvider(props) {
 
             let widthValue = context.modul;
             let barPosition = 0;
+            let isThick = false;
+
+            barsCalculatedValue = context.barsValue * 2 + 1;
 
             if (barType == "thickButton") {
                 widthValue *= 2;
+                isThick = true;
+                barsCalculatedValue++;
             }
 
             if (barsCombination.length > 0) {
@@ -33,25 +42,36 @@ export function BarGenerationCtxProvider(props) {
                     barsCombination[barsCombination.length - 1].position + context.modul;
             }
 
-            let newBar = { value: widthValue, position: barPosition };
-            setBarsCombination(currentBarsCombination => { return currentBarsCombination.concat(newBar) });
-            }
+            let newBar = { value: widthValue, position: barPosition, thick: isThick };
+            setBarsCombination((currentBarsCombination) => { return currentBarsCombination.concat(newBar) });
+        }
         console.log(barsCombination);
+        console.log(barsCalculatedValue);
     };
 
     // to remove a last bar
     function removeLastBarHandler() {
         if (barsCombination.length > 0) {
-            setBarsCombination(currentBarsCombination => { return currentBarsCombination.slice(0, -1) });
+            let currentBarsCombination = [...barsCombination];
+            let lastBar = currentBarsCombination.pop();
+            if (lastBar.thick) {
+                --barsCalculatedValue;
+                
             }
+            barsCalculatedValue = (--barsCalculatedValue) / 2;
+            setBarsCombination(() => currentBarsCombination);
+            // setBarsCombination(currentBarsCombination => { return currentBarsCombination.slice(0, -1) });
+        }
         console.log(barsCombination);
+        // console.log(barsCalculatedValue);
     };
 
     // to remove all bars
     function removeAllBarsHandler() {
         if (barsCombination.length > 0) {
+            barsCalculatedValue = 0;
             setBarsCombination(() => []);
-            }
+        }
         console.log(barsCombination);
     };
 
@@ -62,6 +82,7 @@ export function BarGenerationCtxProvider(props) {
         // distance between bars; it's equal to thin bar width and half of thick bar width
         modul: 10,
         eachBarParams: barsCombination,
+        barsValue: barsCalculatedValue,
         enterNextBar: enterNextBarHandler,
         removeLastBar: removeLastBarHandler,
         removeAllBars: removeAllBarsHandler,
