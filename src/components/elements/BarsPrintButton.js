7e236db3cb82b4527747aad7722e48a3a8310909
sb17018 <piotr.a.bar@gmail.com>
2024-06-18 17:04:09 +0200

    Add throwing bars params to console

--- src/components/elements/BarsPrintButton.js
+++ src/components/elements/BarsPrintButton.js
@@ -1,8 +1,18 @@
+import { useContext } from "react";
+import BarGenerationCtx from "../../js/barGeneration-context";
+
 import selectors from "./BarsPrintButton.module.css";
 
 function BarsPrintButton() {
+    const barsCombCtx = useContext(BarGenerationCtx);
 
-    return <div className={selectors["printing-button"]} id="printingButton"><p>PRINT</p>
+    const barsCombination = barsCombCtx.eachBarParams;
+
+    function print() {
+        console.log(JSON.stringify(barsCombination));
+    }
+
+    return <div className={selectors["printing-button"]} id="printingButton" onClick={print}><p>PRINT</p>
     </div>;
 }
 
