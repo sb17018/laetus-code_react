8a4e85401de6159b95ef271d5982bbdd1a402cc9
sb17018 <piotr.a.bar@gmail.com>
2024-06-18 13:31:55 +0200

    Re-arrange display/calculate elements

--- src/components/panels/InsertNumberPanel.js
+++ src/components/panels/InsertNumberPanel.js
@@ -8,33 +8,17 @@ function InsertNumberPanel() {
 
     const barsValue = barsCombCtx.barsValue;
     
-    const prompt = barsCombCtx.prompt;
-    
     function displaySth(event) {
         barsCombCtx.generateCode(event.target.value);
         barsCombCtx.setPrompt(event.target.value);
     }
-
-    let upperPropmt = <output id={selectors.lessThan3Prompt}></output>
-
-    if (barsValue > 0 && barsValue < 3) {
-        upperPropmt = <output id={selectors.lessThan3Prompt} className={selectors["too-little-number"]}></output>;
-    }
-    else if (prompt) {
-        upperPropmt = <output id={selectors.lessThan3Prompt} className={selectors["too-great-number"]} onAnimationEnd={fadePromptOut}></output>;
-    }
-
-    function fadePromptOut() {
-        setTimeout(() => barsCombCtx.setPrompt(barsValue), 3000);
-    }
-
+    
     return <div>
         <h4>Enter a number to generate code</h4>
         <div id="toEnterNumber">
             <input type="number" id={selectors.userInputField} placeholder="REQUIRED MINIMAL VALUE 3" value={barsValue > 0 ? barsValue : ""} onCopy={() => false} onChange={displaySth} />
         </div>
         <br />
-        {upperPropmt}
     </div>;
 }
 
