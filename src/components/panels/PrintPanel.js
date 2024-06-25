1af6efd252ffec0d4db45bf708e86c69b0740041
sb17018 <piotr.a.bar@gmail.com>
2024-06-25 11:52:14 +0200

    Add sliding in effect to print panel

--- src/components/panels/PrintPanel.js
+++ src/components/panels/PrintPanel.js
@@ -22,17 +22,30 @@ function PrintPanel(props) {
 
     let barCodeSettings = "";
     let barsPrintButtonClass = "";
+    let printPanelClass = "";
     if (barsValue > 2 && barsValue <= MAX_VALUE) {
-        barCodeSettings = <>
-            <InputRadio ref={ref} id="codeMicro" name="codeSize" data-code-type="Micro" value="MICRO" data-code-measure="0.250" className={selectors["code-module-checkbox"]} onChange={checkValue} />
-            <InputRadio ref={ref} id="codeMini" name="codeSize" data-code-type="Mini" value="MINI" data-code-measure="0.375" className={selectors["code-module-checkbox"]} onChange={checkValue} />
-            <InputRadio ref={ref} id="codeStandard" name="codeSize" data-code-type="Standard" value="STANDARD" data-code-measure="0.500" className={selectors["code-module-checkbox"]} onChange={checkValue} defaultChecked="true" />
-        </>;
         barsPrintButtonClass = selectors["print-button-active"];
+        printPanelClass = selectors["panel-visible"];
     }
+    barCodeSettings = <div id={selectors.checkBoxesWrapper}>
+        <InputRadio ref={ref} id="codeMicro" name="codeSize" data-code-type="Micro" value="MICRO" data-code-measure="0.250" className={selectors["code-module-checkbox"]} onChange={checkValue} />
+        <InputRadio ref={ref} id="codeMini" name="codeSize" data-code-type="Mini" value="MINI" data-code-measure="0.375" className={selectors["code-module-checkbox"]} onChange={checkValue} />
+        <InputRadio ref={ref} id="codeStandard" name="codeSize" data-code-type="Standard" value="STANDARD" data-code-measure="0.500" className={selectors["code-module-checkbox"]} onChange={checkValue} defaultChecked="true" />
+    </div>;
+
+    // let barCodeSettings = "";
+    // let barsPrintButtonClass = "";
+    // if (barsValue > 2 && barsValue <= MAX_VALUE) {
+    //     barCodeSettings = <>
+    //         <InputRadio ref={ref} id="codeMicro" name="codeSize" data-code-type="Micro" value="MICRO" data-code-measure="0.250" className={selectors["code-module-checkbox"]} onChange={checkValue} />
+    //         <InputRadio ref={ref} id="codeMini" name="codeSize" data-code-type="Mini" value="MINI" data-code-measure="0.375" className={selectors["code-module-checkbox"]} onChange={checkValue} />
+    //         <InputRadio ref={ref} id="codeStandard" name="codeSize" data-code-type="Standard" value="STANDARD" data-code-measure="0.500" className={selectors["code-module-checkbox"]} onChange={checkValue} defaultChecked="true" />
+    //     </>;
+    //     barsPrintButtonClass = selectors["print-button-active"];
+    // }
 
     function clickFunction(ev) {
-        
+
         ev.preventDefault();
 
         fetch("http://localhost:8080/code",
@@ -46,13 +59,17 @@ function PrintPanel(props) {
             }
         );
     }
-    return <div className={selectors["print-panel"] + " " + selectors[props.className]}>
-        <form onSubmit={clickFunction}>
-            <hr />
-            {barCodeSettings}
-            <Button id={selectors.printingButton} className={barsPrintButtonClass}><p>PRINT</p></Button>
-        </form>
-    </div>;
+    return <>
+        <hr id={selectors.dividerLine}/>
+        <div id={selectors.printPanelWrapper} className={printPanelClass}>
+            <div className={selectors["print-panel"] + " " + printPanelClass}>
+                <form onSubmit={clickFunction}>
+                    {barCodeSettings}
+                    <Button id={selectors.printingButton} className={barsPrintButtonClass}><p>PRINT</p></Button>
+                </form>
+            </div>
+        </div>
+    </>;
 }
 
 export default PrintPanel;
